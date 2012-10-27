52e89e5268d53d02994ba6cf9c7403a6126f2e8a
Sergio Oliveira <sergio@tracy.com.br>
2012-10-27 14:22:44 -0200

    adicionando modelo para itinerario

--- viagem/models.py
+++ viagem/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from ibge.models import Municipio
+
+
+class Itinerario(models.Model):
+    origem = models.ForeignKey(Municipio)
+    destino = models.ForeignKey(Municipio)
+    duracao = models.TimeField()
+
+class Empresa(models.Model):
+    pass
+
+class Viagem(models.Model):
+    pass
