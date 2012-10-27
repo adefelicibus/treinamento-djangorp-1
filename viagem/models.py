3c4141a67241d4563e0f5fe89661b9182f8da560
Sergio Oliveira <sergio@tracy.com.br>
2012-10-27 15:07:35 -0200

    Validacao para nao gravar duas vezes o mesmo itinerario

--- viagem/models.py
+++ viagem/models.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from ibge.models import Municipio
 
@@ -7,6 +9,19 @@ class Itinerario(models.Model):
     origem = models.ForeignKey(Municipio, related_name='origens')
     destino = models.ForeignKey(Municipio, related_name='destinos')
     duracao = models.TimeField()
+
+    def __unicode__(self):
+        return u'{0} - {1}'.format(self.origem, self.destino)
+
+    def save(self, *args, **kwargs):
+        itinerario = Itinerario.objects.filter(
+            origem=self.origem,
+            destino=self.destino,
+        )
+        if not itinerario:
+            super(Itinerario, self).save(*args, **kwargs)
+        else:
+            raise ValidationError('Itinerário já cadastrado')
 
 class Empresa(models.Model):
     pass
