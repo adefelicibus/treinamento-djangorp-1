f7a195acbb22915dec56d17016266b37ea0a0c9a
Sergio Oliveira <sergio@tracy.com.br>
2012-10-28 12:03:12 -0200

    Settings local
    onde eu coloquei aquele comentario legal?!

---
+++ busao/settings_local.py
@@ -0,0 +1,13 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'busao',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+    }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '=qxfrk3nb6@f)+_h^_l&ampasdfhkdsahfj kfhsdbuf7@1bs!b43qs=gmazea6@'
