7538df2d9fa9b2f28d000d4c0b579968738dfcb3
Sergio Oliveira <sergio@tracy.com.br>
2012-10-28 15:36:39 -0200

    Exibindo detalhes da empresa

--- viagem/urls.py
+++ viagem/urls.py
@@ -1,13 +1,17 @@
 
+
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from viagem.views import ListaViagens
-from viagem.models import Viagem
+from viagem.models import Viagem, Empresa
 
 
 urlpatterns = patterns('viagem.views',
     url(r'^$', ListView.as_view(model=Viagem), name='viagens'),
-    url(r'^privado$', login_required(ListaViagens.as_view()), name='viagens'),
+    url(r'^privado$', login_required(ListaViagens.as_view()),
+        name='viagens'),
+    url('^empresa/(?P<slug>[\w_-]+)/?$',
+        DetailView.as_view(model=Empresa), name='empresa'),
 )
