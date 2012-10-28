c367e117c611342ea57b69a7a33bf45042a96cd3
Sergio Oliveira <sergio@tracy.com.br>
2012-10-28 11:09:06 -0200

    Usando generic view no lugar de view com render

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('website.views',
-    url(r'^$', 'home', name='home'),
+    url(r'^$', TemplateView.as_view(
+        template_name='website/home.html'), name='home'),
 )
